f734650223b2494a8cc8ef7746eae4a76f4718bb
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2019-06-12 06:55:25 +0900

    Add adapter for discovered devices

--- app/src/main/java/org/rionlabs/blubot/ui/BLDiscoveryFragment.kt
+++ app/src/main/java/org/rionlabs/blubot/ui/BLDiscoveryFragment.kt
@@ -5,13 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import org.rionlabs.blubot.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.rionlabs.blubot.databinding.FragmentBlDiscoveryBinding
 
 class BLDiscoveryFragment : Fragment() {
+
+    private lateinit var binding: FragmentBlDiscoveryBinding
+
+    private val deviceAdapter = DiscoveredDeviceAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_bl_discovery, container, false)
+        binding = FragmentBlDiscoveryBinding.inflate(inflater, container, false).apply {
+            deviceList.layoutManager = LinearLayoutManager(requireContext())
+            deviceList.adapter = deviceAdapter
+        }
+        return binding.root
     }
 }
---
+++ app/src/main/java/org/rionlabs/blubot/ui/DiscoveredDeviceAdapter.kt
@@ -0,0 +1,42 @@
+package org.rionlabs.blubot.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import org.rionlabs.blubot.DiscoveredDevice
+import org.rionlabs.blubot.databinding.ItemDiscoveredDeviceBinding as ItemBinding
+
+class DiscoveredDeviceAdapter :
+    ListAdapter<DiscoveredDevice, DiscoveredDeviceAdapter.DeviceViewHolder>(DIFF_CALLBACK) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return DeviceViewHolder(ItemBinding.inflate(layoutInflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class DeviceViewHolder(private val binding: ItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(device: DiscoveredDevice) {
+            binding.device = device
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<DiscoveredDevice>() {
+            override fun areItemsTheSame(old: DiscoveredDevice, new: DiscoveredDevice): Boolean {
+                return old.ssid == new.ssid
+            }
+
+            override fun areContentsTheSame(old: DiscoveredDevice, new: DiscoveredDevice): Boolean {
+                return old == new
+            }
+        }
+    }
+}
